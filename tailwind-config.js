b667c11b6c26235e11a7a805c520ff71c27fe605
Key Code <alexhrnndz32@gmail.com>
2026-09-15 21:53:49 -0600

    Update tailwind-config.js

--- tailwind-config.js
+++ tailwind-config.js
@@ -4,9 +4,11 @@ tailwind.config = {
       colors: {
         sky: '#6789A2',
         'sky-light': '#7897AD',
+        'sky-deep': '#4A6B84',
         ivory: '#F0EEDC',
         ink: '#171717',
-        vintage: '#C94B43'
+        vintage: '#C94B43',
+        moss: '#8FB0A0'
       },
       fontFamily: {
         display: ['Caveat', 'cursive'],
